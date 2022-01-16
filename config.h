242f9cbb6da10aa015c18e43af51d0620caf90d8
vuckale <56733276+vuckale@users.noreply.github.com>
2022-01-16 21:40:15 +0100

    map matrix and assign columns and rows to pins

--- config.h
+++ config.h
@@ -3,16 +3,15 @@
 
 #include "config_common.h"
 
-#define VENDOR_ID 0x0000
-#define PRODUCT_ID 0x0000
-#define DEVICE_VER 0x0001
+#define VENDOR_ID       0x049F
+#define PRODUCT_ID      0xB944
+#define DEVICE_VER      0x0002
+#define MANUFACTURER    Vuckale
+#define PRODUCT         Compaq MX 11800 QMK
 
-#define MANUFACTURER "Vuckale"
-#define PRODUCT "MX 11800"
-
-#define MATRIX_ROWS 5
-#define MATRIX_COLS 14
-#define DIODE_DIRECTION COL2ROW
+#define MATRIX_ROWS 14
+#define MATRIX_COLS 9
+#define DIODE_DIRECTION ROW2COL
 
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE 5
--- rules.mk
+++ rules.mk
@@ -4,18 +4,14 @@
 BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = yes        # Console for debug
-COMMAND_ENABLE = yes        # Commands for debug and configuration
+CONSOLE_ENABLE = no        # Console for debug
+COMMAND_ENABLE = no       # Commands for debug and configuration
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
-SLEEP_LED_ENABLE = yes      # Breathing sleep LED during USB suspend
+SLEEP_LED_ENABLE = no      # Breathing sleep LED during USB suspend
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-NKRO_ENABLE = yes           # USB Nkey Rollover
-BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
-BACKLIGHT_DRIVER = software
+NKRO_ENABLE = no           # USB Nkey Rollover
+BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
-OLED_ENABLE = yes
-OLED_DRIVER = SSD1306
-ENCODER_ENABLE = yes
 
 DEFAULT_FOLDER = compaq/mx_11800_vuckale/blackpill_f401
